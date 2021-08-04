a2e731222b4d2a0c31b6e6071122ba4ae9803e4a
Dave Phillips <79615185+ScherzoVivo@users.noreply.github.com>
2021-08-04 11:17:29 -0700

    Add files via upload

---
+++ nametool.py
@@ -0,0 +1,16 @@
+#==================================================#
+# Name Tool Script -- nametool.py --
+# C:\Users\Alleg\Python\UW Course\Week 1\nametool.py
+# Assignment #1 - Display user's name
+# DJP -- 2021-07-09 -- Initial script composition
+#==================================================#
+print("Thank you for using the name query tool!\n")
+
+firstName = input("Please enter your first name...|| ")
+lastName = input("And your last name...|| ")
+
+print("\nSurprisingly, your name is ||", firstName, lastName, "||")
+
+input("\n\nPress Return to terminate")
+
+# nametool.py
